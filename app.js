6f5100cf916bb74182c7c17e56d181f3d4bf8f89
al000sh <ali.ps@hotmail.com>
2020-05-11 03:11:33 +0300

    Lecture6.

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+
+angular.module('myFirstApp', [])
+
+
+.controller('MyFirstController', function($scope) {
+  $scope.name = "Ali"
+  $scope.sayHello = function () { return "Hello Guys!"}
+
+});
+
+})();
